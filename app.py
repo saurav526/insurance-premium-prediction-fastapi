06540283dd76666fe482852e7d4f5bbb064f0388
saurav526 <sauravtiwari2712005@gmail.com>
2026-07-23 20:45:05 +0530

    full runnable code now

--- app.py
+++ app.py
@@ -1,58 +1,43 @@
-from pydantic import BaseModel, Field, computed_field, field_validator
-from typing import Literal, Annotated
-from config.city_tier import tier_1_cities, tier_2_cities
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from schema.user_input import UserInput
+from schema.prediction_response import PredictionResponse
+from Model.predict import predict_output, model, MODEL_VERSION
 
+app = FastAPI()
 
-# pydantic model to validate incoming data
-class UserInput(BaseModel):
+# human readable       
+@app.get('/')
+def home():
+    return {'message':'Insurance Premium Prediction API'}
 
-    age: Annotated[int, Field(..., gt=0, lt=120, description='Age of the user')]
-    weight: Annotated[float, Field(..., gt=0, description='Weight of the user')]
-    height: Annotated[float, Field(..., gt=0, lt=2.5, description='Height of the user')]
-    income_lpa: Annotated[float, Field(..., gt=0, description='Annual salary of the user in lpa')]
-    smoker: Annotated[bool, Field(..., description='Is user a smoker')]
-    city: Annotated[str, Field(..., description='The city that the user belongs to')]
-    occupation: Annotated[Literal['retired', 'freelancer', 'student', 'government_job',
-       'business_owner', 'unemployed', 'private_job'], Field(..., description='Occupation of the user')]
+# machine readable
+@app.get('/health')
+def health_check():
+    return {
+        'status': 'OK',
+        'version': MODEL_VERSION,
+        'model_loaded': model is not None
+    }
+
+@app.post('/predict', response_model=PredictionResponse)
+def predict_premium(data: UserInput):
+
+    user_input = {
+        'bmi': data.bmi,
+        'age_group': data.age_group,
+        'lifestyle_risk': data.lifestyle_risk,
+        'city_tier': data.city_tier,
+        'income_lpa': data.income_lpa,
+        'occupation': data.occupation
+    }
+
+    try:
+
+        prediction = predict_output(user_input)
+
+        return JSONResponse(status_code=200, content={'response': prediction})
     
-    @field_validator('city')
-    @classmethod
-    def normalize_city(cls, v: str) -> str:
-        v = v.strip().title()
-        return v
-    
-    @computed_field
-    @property
-    def bmi(self) -> float:
-        return self.weight/(self.height**2)
-    
-    @computed_field
-    @property
-    def lifestyle_risk(self) -> str:
-        if self.smoker and self.bmi > 30:
-            return "high"
-        elif self.smoker or self.bmi > 27:
-            return "medium"
-        else:
-            return "low"
-        
-    @computed_field
-    @property
-    def age_group(self) -> str:
-        if self.age < 25:
-            return "young"
-        elif self.age < 45:
-            return "adult"
-        elif self.age < 60:
-            return "middle_aged"
-        return "senior"
-    
-    @computed_field
-    @property
-    def city_tier(self) -> int:
-        if self.city in tier_1_cities:
-            return 1
-        elif self.city in tier_2_cities:
-            return 2
-        else:
-            return 3
+    except Exception as e:
+
+        return JSONResponse(status_code=500, content=str(e))
